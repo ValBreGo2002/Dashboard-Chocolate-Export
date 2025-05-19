b15172fe730858bd6b4810849d5f7951e4f70231
ValBreGo2002 <valery.brenes.gomez@est.una.ac.cr>
2025-05-19 11:00:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+```
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -23,7 +24,8 @@ st.dataframe(clientes_filtrados)
 st.subheader(" Exportaciones de Chocolates")
 exportaciones_filtradas = exportaciones[exportaciones["País"] == pais_seleccionado]
 fig, ax = plt.subplots()
-ax.bar(exportaciones_filtradas["País"], exportaciones_filtradas["Exportaciones (USD millones)"], color='#2E86C1')
+ax.bar(exportaciones_filtradas["País"], exportaciones_filtradas["Exportaciones (USD
+millones)"], color='#2E86C1')
 ax.set_xlabel("País")
 ax.set_ylabel("Exportaciones (USD millones)")
 ax.set_title(f"Exportaciones de Chocolates en {pais_seleccionado}")
@@ -47,3 +49,5 @@ ax2.set_ylabel("Tamaño del Mercado (USD millones)")
 ax2.set_title("Comparación de Tamaños de Mercado")
 plt.xticks(rotation=45)
 st.pyplot(fig2)
+```
+
